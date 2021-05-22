b056e4862a631cf823d6dfe2f9defc990923e7a4
Moumen Hallak <moumen95@gmail.com>
2021-05-22 08:19:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your Circle class here
+
+class Circle {
+  constructor(radius){
+    this.radius = radius;
+  }
+  //   set accMeasurement(accMeasurement){
+  //   return 2 * Math.PI * radius;
+  // }
+
+  get diameter() {
+    return this.radius * 2;
+  }
+  get circumference() {
+    return 2 * Math.PI * this.radius;
+  }
+  get area(){
+    return Math.PI * (this.radius * this.radius);
+  }
+  
+  //   set diameter(diameter) {
+  //   this.radius =  diameter * 2;
+  // }
+  // set circumference(circumference) {
+  //   this.radius = Math.PI * diameter;
+  // }
+  // set area(area){
+  //   this.radius = Math.PI * (area * area);
+  // }
+
+  
+}
